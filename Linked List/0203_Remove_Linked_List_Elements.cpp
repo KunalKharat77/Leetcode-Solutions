ef2e651356b3b2887df8b9f517be362ea6766fa8
KunalKharat77 <kunalkharat2008@gmail.com>
2026-08-19 22:37:20 +0530

    Add Linked List Solution

---
+++ Linked List/0203_Remove_Linked_List_Elements.cpp
@@ -0,0 +1,21 @@
+// 0203_Remove_Linked_List_Elements.cpp
+
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode dummy(0);
+        dummy.next = head;
+
+        ListNode* curr = &dummy;
+
+        while(curr->next) {
+            if(curr->next->val == val) {
+                curr->next = curr->next->next;
+            } else {
+                curr = curr->next;
+            }
+        }
+
+        return dummy.next;
+    }
+};
