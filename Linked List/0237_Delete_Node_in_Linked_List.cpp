6d9f7bae718cf885e9670852e561375a55af2eb1
KunalKharat77 <kunalkharat2008@gmail.com>
2026-08-19 22:37:59 +0530

    Add Linked List Solution

---
+++ Linked List/0237_Delete_Node_in_Linked_List.cpp
@@ -0,0 +1,19 @@
+// 0237_Delete_Node_in_Linked_List.cpp
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* nextNode = node->next;
+
+        node->val = nextNode->val;
+        node->next = nextNode->next;
+    }
+};
