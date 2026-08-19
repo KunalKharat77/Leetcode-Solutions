343ad0fd54c10ebecefd425abac2abe014fc7663
KunalKharat77 <kunalkharat2008@gmail.com>
2026-08-19 22:35:46 +0530

    Add Linked List Solution

---
+++ Linked List/0083_Remove_Duplicates_from_Sorted_List.cpp
@@ -0,0 +1,36 @@
+// 0083_Remove_Duplicates_from_Sorted_List.cpp
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(!head) return head;
+
+        ListNode* left = head;
+        ListNode* right = head ? head->next : nullptr;
+
+        while(right){
+            if(left->val == right->val){
+                right = right->next;
+            }
+            else{
+                left->next = right;
+                left = right;
+                right = right->next;
+            }
+        }
+
+        left->next = NULL;
+
+        return head;
+    }
+};
