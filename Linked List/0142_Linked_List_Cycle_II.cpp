47da3b8d8cfd65f3e6b262c3abc945c47d848f54
KunalKharat77 <kunalkharat2008@gmail.com>
2026-08-19 22:36:38 +0530

    Add Linked List Solution

---
+++ Linked List/0142_Linked_List_Cycle_II.cpp
@@ -0,0 +1,37 @@
+// 0142_Linked_List_Cycle_II.cpp
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* detectCycle(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        if(!fast || !fast->next){
+            return NULL;
+        }
+
+        ListNode* ptr = head;
+        while(ptr != slow){
+            slow = slow->next;
+            ptr = ptr->next;
+        }
+        
+        return ptr;
+    }
+};
