b2b68ed6e60e3b17b76887c694f552b800f87c32
KunalKharat77 <kunalkharat2008@gmail.com>
2026-08-21 23:49:09 +0530

    Added Trees Solution

---
+++ Trees/0111_Minimum_Depth_of_Binary_Tree.cpp
@@ -0,0 +1,27 @@
+// 0111_Minimum_Depth_of_Binary_Tree.cpp
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root == NULL) return 0;
+
+        int left = minDepth(root->left);
+        int right = minDepth(root->right);
+
+        if(left == NULL) return 1 + right;
+        if(right == NULL) return 1 + left;
+
+        return 1 + min(left, right);
+    }
+};
