969031028aa8c38cbc202f6cda5d6c4ece549594
KunalKharat77 <kunalkharat2008@gmail.com>
2026-08-21 23:51:27 +0530

    Added Trees Solution

---
+++ Trees/0543_Diameter_of_Binary_Tree.cpp
@@ -0,0 +1,35 @@
+// 0543_Diameter_of_Binary_Tree.cpp
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int maxDiameter = 0;
+        calculateHeight(root, maxDiameter);
+        return maxDiameter;
+    }
+
+private:
+    int calculateHeight(TreeNode* node, int& maxDiameter) {
+        if (!node) return 0;
+
+        int leftHeight = calculateHeight(node->left, maxDiameter);
+        int rightHeight = calculateHeight(node->right, maxDiameter);
+
+        // Update the global maximum diameter
+        maxDiameter = max(maxDiameter, leftHeight + rightHeight);
+
+        // Return height of the current subtree
+        return 1 + max(leftHeight, rightHeight);
+    }
+};
