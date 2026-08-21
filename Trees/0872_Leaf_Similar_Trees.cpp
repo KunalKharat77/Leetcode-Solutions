82e2b97b2976c35915f5db2f6472e16dc31368fd
KunalKharat77 <kunalkharat2008@gmail.com>
2026-08-21 23:52:01 +0530

    Added Trees Solution

---
+++ Trees/0872_Leaf_Similar_Trees.cpp
@@ -0,0 +1,39 @@
+// 0872_Leaf_Similar_Trees.cpp
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void getLeaf(TreeNode* root, vector<int>& vec) {
+        if (root == nullptr)
+            return;
+
+        if(root->left == nullptr && root->right == nullptr){
+            vec.push_back(root->val);
+            return;
+        }
+
+        getLeaf(root->left, vec);
+        getLeaf(root->right, vec);
+    }
+
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int> first;
+        vector<int> second;
+
+        getLeaf(root1, first);
+        getLeaf(root2, second);
+
+        return first == second;
+    }
+};
